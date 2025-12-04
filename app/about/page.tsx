73715f5ac0d6fcfabf14031e1fafd33c6084b44b
Creativecoder11 <creativecoderpro@gmaol.com>
2025-12-04 21:26:22 +0600

    WEbsite Update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -16,7 +16,7 @@ export default function About() {
       <CoreValuesSection />
       <TeamSection />
       <Faq />
-      {/* <CircularCarousel /> */}
+      <CircularCarousel />
     </div>
   )
 }
